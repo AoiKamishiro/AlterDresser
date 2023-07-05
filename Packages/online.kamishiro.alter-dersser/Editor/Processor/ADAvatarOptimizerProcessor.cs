fa399a2576f9020dec50d21878db02da3f7ed244
AoiKamishiro <40717483+AoiKamishiro@users.noreply.github.com>
2023-07-05 18:40:12 +0900

    ProcessorFix

--- Packages/online.kamishiro.alter-dersser/Editor/Processor/ADAvatarOptimizerProcessor.cs
+++ Packages/online.kamishiro.alter-dersser/Editor/Processor/ADAvatarOptimizerProcessor.cs
@@ -13,59 +13,58 @@ namespace online.kamishiro.alterdresser.editor
     {
         internal static void ProcessMergeMesh(ADSEnhanced item, ADBuildContext context)
         {
-            if (item.doMergeMesh)
+            if (!item.doMergeMesh) return;
+
+            AvatarObjectReference avatarObjectReference = new AvatarObjectReference
             {
-                AvatarObjectReference avatarObjectReference = new AvatarObjectReference
+                referencePath = ADRuntimeUtils.GetRelativePath(ADRuntimeUtils.GetAvatar(item.transform).transform, context.enhancedRootBone.transform)
+            };
+
+            GameObject mergedMesh = new GameObject("MergedMesh");
+            mergedMesh.transform.SetParent(item.transform);
+
+            Component mergeMeshComponent = mergedMesh.AddComponent(ADAvaterOptimizer.MergeMeshType);
+            SerializedObject serializedMergeMesh = new SerializedObject(mergeMeshComponent);
+            serializedMergeMesh.Update();
+
+            SerializedProperty renderersSet = serializedMergeMesh.FindProperty("renderersSet").FindPropertyRelative("mainSet");
+
+            List<Renderer> validChildRenderers = item.GetComponentsInChildren<Renderer>()
+                .Where(x => (x is SkinnedMeshRenderer || x is MeshRenderer) && !context.meshRendererBackup.Select(y => y.smr).Contains(x))
+                .Select(x =>
                 {
-                    referencePath = ADRuntimeUtils.GetRelativePath(ADRuntimeUtils.GetAvatar(item.transform).transform, context.enhancedRootBone.transform)
-                };
+                    MeshRendererBuckup backup = context.meshRendererBackup.FirstOrDefault(y => y.renderer == x);
+                    return backup != null ? backup.smr : x;
+                })
+                .Where(x => x != mergedMesh.GetComponent<SkinnedMeshRenderer>())
+                .ToList();
 
-                IEnumerable<SkinnedMeshRenderer> renderer = item.GetComponentsInChildren<SkinnedMeshRenderer>(true).Where(x => x != null);
-                GameObject mergedMesh = new GameObject("MergedMesh");
-                ADEditorUtils.SaveGeneratedItem(mergedMesh, context);
-                mergedMesh.transform.SetParent(item.transform);
-                Component m = mergedMesh.AddComponent(ADAvaterOptimizer.MergeMeshType);
-                SerializedObject so = new SerializedObject(m);
-                so.Update();
-                SerializedProperty renderersSet = so.FindProperty("renderersSet").FindPropertyRelative("mainSet");
+            char[] bin = Convert.ToString(item.mergeMeshIgnoreMask, 2).PadLeft(validChildRenderers.Count, '0').ToCharArray();
 
-                IEnumerable<Renderer> childR = item.GetComponentsInChildren<Renderer>()
-                    .Where(x => (x is SkinnedMeshRenderer || x is MeshRenderer) && !context.meshRendererBackup.Select(y => y.smr).Contains(x))
-                    .Select(x =>
-                    {
-                        MeshRendererBuckup backup = context.meshRendererBackup.FirstOrDefault(y => y.renderer == x);
-                        return backup != null ? backup.smr : x;
-                    });
+            Enumerable.Range(0, validChildRenderers.Count)
+                .Where(i => bin[i] == '0')
+                .ToList()
+                .ForEach(i =>
+                {
+                    renderersSet.InsertArrayElementAtIndex(renderersSet.arraySize);
+                    renderersSet.GetArrayElementAtIndex(renderersSet.arraySize - 1).objectReferenceValue = validChildRenderers[i];
+                });
 
-                char[] bin = Convert.ToString(item.mergeMeshIgnoreMask, 2).PadLeft(childR.Count(), '0').ToCharArray();
+            serializedMergeMesh.ApplyModifiedProperties();
 
-                Enumerable.Range(0, childR.Count())
-                    .Where(i => bin[i] == '0')
-                    .ToList()
-                    .ForEach(i =>
-                    {
-                        renderersSet.InsertArrayElementAtIndex(renderersSet.arraySize);
-                        renderersSet.GetArrayElementAtIndex(renderersSet.arraySize - 1).objectReferenceValue = childR.ElementAt(i);
-                    });
+            ModularAvatarMeshSettings maMeshSettings = mergedMesh.AddComponent<ModularAvatarMeshSettings>();
+            maMeshSettings.InheritBounds = ModularAvatarMeshSettings.InheritMode.Set;
+            maMeshSettings.RootBone = avatarObjectReference;
+            maMeshSettings.Bounds = new Bounds(ADRuntimeUtils.GetAvatar(item.transform).ViewPosition / 2, new Vector3(2.5f, 2.5f, 2.5f));
 
-                so.ApplyModifiedProperties();
-
-                ModularAvatarMeshSettings maMeshSettings = mergedMesh.AddComponent<ModularAvatarMeshSettings>();
-                maMeshSettings.InheritBounds = ModularAvatarMeshSettings.InheritMode.Set;
-                maMeshSettings.RootBone = avatarObjectReference;
-                maMeshSettings.Bounds = new Bounds(ADRuntimeUtils.GetAvatar(item.transform).ViewPosition / 2, new Vector3(2.5f, 2.5f, 2.5f));
-                ADEditorUtils.SaveGeneratedItem(maMeshSettings, context);
-            }
+            ADEditorUtils.SaveGeneratedItem(mergedMesh, context);
+            ADEditorUtils.SaveGeneratedItem(maMeshSettings, context);
         }
         internal static void ProcessFreezeBlendshape(ADSBlendshape item, ADBuildContext context)
         {
             if (item.doFleezeBlendshape && item.TryGetComponent(out SkinnedMeshRenderer smr) && smr.sharedMesh && smr.sharedMesh.blendShapeCount > 0)
             {
-                string binaryNumber = Convert.ToString(item.fleezeBlendshapeMask, 2);
-                while (smr.sharedMesh.blendShapeCount - binaryNumber.Length > 0)
-                {
-                    binaryNumber = "0" + binaryNumber;
-                }
+                string binaryNumber = Convert.ToString(item.fleezeBlendshapeMask, 2).PadLeft(smr.sharedMesh.blendShapeCount, '0');
                 IEnumerable<bool> mask = binaryNumber.ToCharArray().Select(x => x != '1');
 
                 Component m = item.gameObject.AddComponent(ADAvaterOptimizer.FreezeBlendShapeType);
@@ -74,27 +73,28 @@ namespace online.kamishiro.alterdresser.editor
                 SerializedProperty shapeKeysSet = so.FindProperty("shapeKeysSet").FindPropertyRelative("mainSet");
 
                 IEnumerable<string> usingBlendshapes = ADRuntimeUtils.GetAvatar(item.transform).GetComponentsInChildren<AlterDresserMenuItem>(true)
-                       .SelectMany(x => x.adElements)
-                       .Where(x => x.mode == SwitchMode.Blendshape)
-                       .Where(x => x.objRefValue == item)
-                       .SelectMany(x => ADSwitchBlendshapeEditor.GetUsingBlendshapeNames(x))
-                       .Distinct();
+                    .SelectMany(x => x.adElements)
+                    .Where(x => x.mode == SwitchMode.Blendshape)
+                    .Where(x => x.objRefValue == item)
+                    .SelectMany(x => ADSwitchBlendshapeEditor.GetUsingBlendshapeNames(x))
+                    .Distinct();
 
                 if (item.TryGetComponent(out ModularAvatarBlendshapeSync ma))
                 {
                     usingBlendshapes = usingBlendshapes.Concat(ma.Bindings.Select(x => x.LocalBlendshape != string.Empty ? x.LocalBlendshape : x.Blendshape)).Distinct();
                 }
 
-                IEnumerable<string> ignoreBlendshapes = Enumerable.Range(0, smr.sharedMesh.blendShapeCount)
-                    .Where(x => mask.ElementAt(x))
-                    .Select(x => smr.sharedMesh.GetBlendShapeName(x))
-                    .Distinct()
-                    .Except(usingBlendshapes);
+                IEnumerable<string> blendShapeNames = Enumerable.Range(0, smr.sharedMesh.blendShapeCount)
+                    .Where(mask.ElementAt)
+                    .Select(smr.sharedMesh.GetBlendShapeName)
+                    .Distinct();
 
-                foreach (string i in Enumerable.Range(0, smr.sharedMesh.blendShapeCount).Where(x => mask.ElementAt(x)).Select(x => smr.sharedMesh.GetBlendShapeName(x)).Distinct().Except(usingBlendshapes))
+                IEnumerable<string> ignoreBlendshapes = blendShapeNames.Except(usingBlendshapes);
+
+                foreach (string blendShapeName in ignoreBlendshapes)
                 {
                     shapeKeysSet.InsertArrayElementAtIndex(shapeKeysSet.arraySize);
-                    shapeKeysSet.GetArrayElementAtIndex(shapeKeysSet.arraySize - 1).stringValue = i;
+                    shapeKeysSet.GetArrayElementAtIndex(shapeKeysSet.arraySize - 1).stringValue = blendShapeName;
                 }
 
                 so.ApplyModifiedProperties();
